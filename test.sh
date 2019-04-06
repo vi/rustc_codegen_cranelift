29b98ff4ce1f3b34d02b889a3bc070daf95b1125
bjorn3 <bjorn3@users.noreply.github.com>
2019-04-06 17:07:52 +0200

    Revert "Use -Cprefer-dynamic for tests again"
    This reverts commit 92867df35ec2a28124952b063694665766ad9207.

--- test.sh
+++ test.sh
@@ -50,7 +50,7 @@ $RUSTC example/std_example.rs --crate-type bin
 git clone https://github.com/rust-lang/rust.git --depth=1 || true
 cd rust
 git checkout -- .
-git pull
+#git pull
 export RUSTFLAGS=
 
 cat > the_patch.patch <<EOF
@@ -64,7 +64,7 @@ Subject: [PATCH] Make suitable for cg_clif tests
  1 files changed, 0 insertions(+), 10 deletions(-)
 
 diff --git a/.gitmodules b/.gitmodules
-index b75e312d..aef8bc14 100644
+index 31db0772cfb..5832ba2578b 100644
 --- a/.gitmodules
 +++ b/.gitmodules
 @@ -25,9 +25,6 @@
@@ -77,7 +77,7 @@ index b75e312d..aef8bc14 100644
  [submodule "src/stdarch"]
  	path = src/stdarch
  	url = https://github.com/rust-lang/stdarch.git
-@@ -40,10 +34,6 @@
+@@ -37,10 +34,6 @@
  [submodule "src/doc/edition-guide"]
  	path = src/doc/edition-guide
  	url = https://github.com/rust-lang/edition-guide.git
@@ -88,6 +88,86 @@ index b75e312d..aef8bc14 100644
  [submodule "src/doc/embedded-book"]
  	path = src/doc/embedded-book
  	url = https://github.com/rust-embedded/book.git
+diff --git a/src/liballoc/lib.rs b/src/liballoc/lib.rs
+index ddfa6797a57..94379afc2bd 100644
+--- a/src/liballoc/lib.rs
++++ b/src/liballoc/lib.rs
+@@ -116,7 +116,7 @@
+ #![feature(unsize)]
+ #![feature(unsized_locals)]
+ #![feature(allocator_internals)]
+-#![cfg_attr(bootstrap, feature(on_unimplemented))]
++#![feature(on_unimplemented)]
+ #![feature(rustc_const_unstable)]
+ #![feature(slice_partition_dedup)]
+ #![feature(maybe_uninit_extra, maybe_uninit_slice)]
+diff --git a/src/libcore/lib.rs b/src/libcore/lib.rs
+index ca431627147..1b67b05c730 100644
+--- a/src/libcore/lib.rs
++++ b/src/libcore/lib.rs
+@@ -89,7 +89,7 @@
+ #![feature(nll)]
+ #![feature(exhaustive_patterns)]
+ #![feature(no_core)]
+-#![cfg_attr(bootstrap, feature(on_unimplemented))]
++#![feature(on_unimplemented)]
+ #![feature(optin_builtin_traits)]
+ #![feature(prelude_import)]
+ #![feature(repr_simd, platform_intrinsics)]
+diff --git a/src/libstd/lib.rs b/src/libstd/lib.rs
+index 927fd2a6b0b..c7adad896a5 100644
+--- a/src/libstd/lib.rs
++++ b/src/libstd/lib.rs
+@@ -284,7 +284,7 @@
+ #![feature(never_type)]
+ #![feature(nll)]
+ #![cfg_attr(bootstrap, feature(non_exhaustive))]
+-#![cfg_attr(bootstrap, feature(on_unimplemented))]
++#![feature(on_unimplemented)]
+ #![feature(optin_builtin_traits)]
+ #![feature(panic_info_message)]
+ #![feature(panic_internals)]
+diff --git a/src/tools/compiletest/src/main.rs b/src/tools/compiletest/src/main.rs
+index 34435819a2c..b115539b4af 100644
+--- a/src/tools/compiletest/src/main.rs
++++ b/src/tools/compiletest/src/main.rs
+@@ -568,6 +568,7 @@ pub fn test_opts(config: &Config) -> test::TestOpts {
+         skip: vec![],
+         list: false,
+         options: test::Options::new(),
++        time_options: None,
+     }
+ }
+
+@@ -703,6 +704,7 @@ pub fn make_test(config: &Config, testpaths: &TestPaths) -> Vec<test::TestDescAn
+                     ignore,
+                     should_panic,
+                     allow_fail: false,
++                    test_type: test::TestType::Unknown,
+                 },
+                 testfn: make_test_closure(config, early_props.ignore, testpaths, revision),
+             }
+diff --git a/src/tools/compiletest/src/runtest.rs b/src/tools/compiletest/src/runtest.rs
+index a9acc9733c1..302b7ef8e76 100644
+--- a/src/tools/compiletest/src/runtest.rs
++++ b/src/tools/compiletest/src/runtest.rs
+@@ -1798,6 +1798,7 @@ impl<'test> TestCx<'test> {
+                 || self.config.target.contains("wasm32")
+                 || self.config.target.contains("nvptx")
+                 || self.is_vxworks_pure_static()
++                || true
+             {
+                 // We primarily compile all auxiliary libraries as dynamic libraries
+                 // to avoid code size bloat and large binaries as much as possible
+@@ -2033,7 +2034,7 @@ impl<'test> TestCx<'test> {
+                 || self.is_vxworks_pure_static() {
+                 // rustc.arg("-g"); // get any backtrace at all on errors
+             } else if !self.props.no_prefer_dynamic {
+-                rustc.args(&["-C", "prefer-dynamic"]);
++                // rustc.args(&["-C", "prefer-dynamic"]);
+             }
+         }
+
 --
 2.11.0
 
@@ -115,6 +195,6 @@ done
 echo "[TEST] run-pass"
 
 #rm -r build/x86_64-unknown-linux-gnu/test || true
-./x.py test --stage 0 src/test/run-pass/ \
-    --rustc-args "-Zcodegen-backend=$(pwd)/../target/"$channel"/librustc_codegen_cranelift."$dylib_ext" --sysroot $(pwd)/../build_sysroot/sysroot -Cpanic=abort" \
+./x.py test --stage 0 src/test/ui/ \
+    --rustc-args "-Zcodegen-backend=$(pwd)/../target/"$CHANNEL"/librustc_codegen_cranelift."$dylib_ext" --sysroot $(pwd)/../build_sysroot/sysroot -Cpanic=abort" \
     2>&1 | tee log.txt
